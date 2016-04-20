ba24877d4152d4fdefdad03e21ff27488fd1ad83
Robin <robinjghartman@gmail.com>
2016-04-20 13:06:11 +0200

    Exploratory Data Analysis

--- plot1.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-data <- fread("household_power_consumption.txt", na.strings = "?")
-
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
-
-data2 <- data[grep(("2007-02-01|2007-02-02"), data$Date)]
-png("plot1.png", width=480, height=480)
-hist(data2$Global_active_power, col = "orangered", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-dev.off()
